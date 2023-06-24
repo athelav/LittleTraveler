727034a5034000869c6b71c869d242b6657fd03e
athelav <athelavill0709@gmail.com>
2023-06-23 22:50:21 -0400

    Formatting and animations to title screen

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=8080, threaded=True)
